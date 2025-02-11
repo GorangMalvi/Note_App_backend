7b4c120667d88f78c099491d7bfaaff48f2fa466
unknown <Gorangmalvi34@gmail.com>
2025-02-11 13:29:54 +0530

    cors updated for vercel

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://notesapp-teal-kappa.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true,
   }));
